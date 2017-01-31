3b17d44dfa7f72b20b53bd12973144349281bef5
Erwan de Bénazé <duhildebenaze@intechinfo.fr>
2017-01-31 14:31:08 +0100

    Avancée sur les commentaire

--- it-network-app/src/app/components/post-comment/post-comment.ts
+++ it-network-app/src/app/components/post-comment/post-comment.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Comment, Post } from 'models';
-import { PostService } from 'services';
+import { PostService, MessageParser } from 'services';
 
 @Component({
     templateUrl: 'post-comment.html',
@@ -13,16 +13,27 @@ export class PostCommentComponent {
     @Input() post: Post;
 
     constructor(
-        private postService: PostService
+        private postService: PostService,
+        private parser: MessageParser
     ) { }
 
-    commentPost(){
+    commentPost() {
+        this.post.content = this.parser.parse(this.post);
+        if (this.post.content) {
+            const regex = /(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gmi;
+            const urlContent = regex.exec(this.post.message);
+
+            if (urlContent) {
+                var messageReplaced = this.post.message.replace(urlContent[0], "");
+                this.post.message = messageReplaced;
+            }
+        }
         this.postService.comment(this.post, this.comment)
-        .then( (result) => {
-            this.comment = "";
-        })
-        .catch((error) => {
-            this.error = "Le commentaire n'a pas fonctionné";
-        })
+            .then((result) => {
+                this.comment = "";
+            })
+            .catch((error) => {
+                this.error = "Le commentaire n'a pas fonctionné";
+            })
     }
 }
